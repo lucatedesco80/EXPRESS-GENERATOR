d890a78eab7caf3486ab5326261ffa40f5a6c1eb
lucatedesco80 <lucatedesco80@gmail.com>
2019-12-10 12:41:39 +0000

    03.1.1

---
+++ EXPRESS_GENERATOR/routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/ciaone', function(req, res, next) {
+  res.send('PRONTONE');
+});
+
+module.exports = router;
